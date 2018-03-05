6c5ba251bd52fb11af8553dd4294f764a14ffbcf
SinfulCitrus <kanexer@gmail.com>
2018-03-05 13:41:05 +0000

    timing + optimisation

--- fullFile.py
+++ fullFile.py
@@ -1,7 +1,10 @@
 import urllib.request
 import sys
 import requests
+import time
 from bs4 import BeautifulSoup as bs
+
+time_test = 0
 
 def extract_source(url):
     # User agent for the website
@@ -14,11 +17,14 @@ def extract_data(source):
     s_page = bs(source.content, "lxml")
     linksFound = 0
     imageExists = 0 # only looks for the highest res image
-
+    
+    if time_test == 1:
+        t0 = time.time()
+    
     # images are delivered through JS
-    for link in s_page.findAll("script"):
+    for link in s_page.findAll("script",{"class":"modelExport"}):
         for keyword in ['k','h','l','c','z','n','m','s','t','q']:
-
+            print("Entered")
             # look for a specific keyword in the sites source code
             strg = str(link)
             res = strg.find("\""+keyword+"\":")
@@ -38,6 +44,9 @@ def extract_data(source):
 
         if imageExists == 1:
             break
+    
+    if time_test == 1:
+        print("\nTime: ",time.time()-t0)
 
     print("\nImage links found: ",linksFound)
 
